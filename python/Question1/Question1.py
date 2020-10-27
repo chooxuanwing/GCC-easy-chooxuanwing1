c5cf9ad441dd8c540ddd0f45f9ba64d29e4b3a41
Wing <chooxuanwing@gmail.com>
2020-10-27 15:14:22 +0000

    solved 1

--- python/Question1/Question1.py
+++ python/Question1/Question1.py
@@ -1,18 +1,26 @@
 import sys
 # You may change this function parameters
+
 def findMaxProfit(numOfPredictedDay, predictedSharePrices):
     # Participants code will be here
-    print(numOfPredictedDay,predictedSharePrices)
+    # print(numOfPredictedDay,predictedSharePrices)
+    temp=0
+
+    for i in range(0,len(predictedSharePrices)):
+        for j in range(i,len(predictedSharePrices)):
+            if (predictedSharePrices[j]-predictedSharePrices[i] > temp):
+                temp=predictedSharePrices[j]-predictedSharePrices[i]
+                print(temp)
 
 
-    return 0
+    return temp
 
 
 def main():
     line = input().split()
-    numOfPredictedDay = 14 ##int(line[0])
-    predictedSharePrices = [5,1,6,3,2,5,6,1,3,6,2,5,5,10] ##list(map(int, line[1:]))
-
+    numOfPredictedDay =  int(line[0])
+    predictedSharePrices = list(map(int, line[1:]))
+    print('hello')
     answer = findMaxProfit(numOfPredictedDay, predictedSharePrices)
 
     # Please do not remove the below line.
