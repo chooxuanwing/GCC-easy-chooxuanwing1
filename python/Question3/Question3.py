bd3414e2d669be0f75d9252b8aad753138285ab7
Wing <chooxuanwing@gmail.com>
2020-10-27 17:27:31 +0000

    fixed +1

--- python/Question3/Question3.py
+++ python/Question3/Question3.py
@@ -17,23 +17,29 @@ def find_min_days(prices, profit):
         for x in range(0,len(prices)):
             for y in range(x,len(prices)):
                 # print(prices[y], '-', prices[x])
+                count+=1
                 if (prices[y]-prices[x]==i and y-x < days):
-                    count+=1
+                    
                     days=y-x
-                    temp.append([x,y])
-
-        out=out+str(temp[-1][0]) +" " + str(temp[-1][1])
+                    temp.append(str(x+1)+" "+str(y+1))
+                elif (count == len(prices)):
+                    temp.append(str(-1))
+                    continue
+        # print(temp)       
+        out = out + temp[-1]  
+        # print(out)
+        # out=out+str(temp[-1][0]) +" " + str(temp[-1][1])
        
     return out
 
-n, d = map(int, input().split())
-prices = list(map(int, input().split()))
-profit = list()
+# n, d = map(int, input().split())
+# prices = list(map(int, input().split()))
+# profit = list()
 # d=2
-# profit=[3,2]
-# prices = [3,1,2,1,4,5]
-for i in range(d):
-    profit.append(int(input().strip()))
+profit=[3,2]
+prices = [3,1,2,1,4,5]
+# for i in range(d):
+#     profit.append(int(input().strip()))
 answer = find_min_days(prices,profit)
 # Do not remove below line
 print(answer)
