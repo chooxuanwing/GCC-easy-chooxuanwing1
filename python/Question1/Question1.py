45c87a53a0d7af9876b8245c08b6488d725d8c40
Wing <chooxuanwing@gmail.com>
2020-10-27 15:12:59 +0000

    solved 1

--- python/Question1/Question1.py
+++ python/Question1/Question1.py
@@ -2,19 +2,22 @@ import sys
 # You may change this function parameters
 def findMaxProfit(numOfPredictedDay, predictedSharePrices):
     # Participants code will be here
-    return -1
+    print(numOfPredictedDay,predictedSharePrices)
+
+
+    return 0
 
 
 def main():
     line = input().split()
-    numOfPredictedDay = int(line[0])
-    predictedSharePrices = list(map(int, line[1:]))
+    numOfPredictedDay = 14 ##int(line[0])
+    predictedSharePrices = [5,1,6,3,2,5,6,1,3,6,2,5,5,10] ##list(map(int, line[1:]))
 
     answer = findMaxProfit(numOfPredictedDay, predictedSharePrices)
 
     # Please do not remove the below line.
     print(answer)
-    # Do not print anything after this line
+    # Do not print anything after this line1
 
 if __name__ == '__main__':
     main()
